6696591dc2243169ee29ada447b1266ed67cbfdf
Schlueter <bs@schlueter.blue>
2015-11-27 01:36:27 -0500

    Simplify primes.PrimeSieve

--- python/util/primes.py
+++ python/util/primes.py
@@ -8,14 +8,10 @@ class PrimeSieve:
         self.__calculate_primacies(range(3, initial_size, 2))
 
     def __calculate_primacies(self, candidates):
-        for prime_candidate in candidates:
-            prime = prime_candidate
+        while candidates:
+            prime = candidates.pop(0)
             self.primes.append(prime)
-            prime_index = candidates.index(prime)
-            composite_candidates = candidates[prime_index + 1:]
-            to_be_removed = filter(lambda x: x % prime == 0, composite_candidates)
-            for candidate in to_be_removed:
-                candidates.remove(candidate)
+            candidates = filter(lambda x: x % prime != 0, candidates)
 
 if __name__ == "__main__":
     print PrimeSieve(int(argv[1])).primes[int(argv[2])]
